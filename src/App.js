68fc33bd95af006d30cb6d5637632927fe7d8940
yooper <dcardin2007@gmail.com>
2023-01-09 08:05:28 -0500

    basic map working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import './index.css';
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
 
-function App() {
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
+export default function App() {
+
+  const position = [51.505, -0.09]
+  const [coordinates, setCoordinates] = useState(null)
+
+    useEffect(() => {
+      async function fetchData() {
+          const response = await fetch('http://127.0.0.1:9906/content/fetch-all')
+          if (response.ok) {
+              const data = await response.json();
+              let positions = []
+              data.Records.forEach(record => {
+                  if (record.ContentLatitude && record.ContentLongitude) {
+                      record.Position = [record.ContentLatitude, record.ContentLongitude]
+                      positions.push(record)
+                  }
+              })
+              setCoordinates(positions);
+          }
+      }
+      fetchData();
+
+    }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <MapContainer
+              center={position}
+              zoom={1}
+              scrollWheelZoom={true}
+              style={{ height: "100vh" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+              {coordinates &&
+                coordinates.map((record) => (
+                <Marker position={record.Position}>
+                  <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                  </Popup>
+                </Marker>
+                ))}
+          </MapContainer>
+  )
 }
-
-export default App;
